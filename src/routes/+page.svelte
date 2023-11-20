fed6c12344eeaae3cec02bb36a1a68c4047e7be2
Adam Teo <adam.teo321@gmail.com>
2023-11-19 21:46:01 -0600

    marquee init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Marquee from "$lib/components/Marquee.svelte";
+
+</script>
+
+<div class="flex flex-col">
+    <Marquee />
+</div>
