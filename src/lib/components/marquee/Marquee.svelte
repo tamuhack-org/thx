3b0c5c0b1916b078845d4302d94cd9ed00ccc6da
Adam Teo <adam.teo321@gmail.com>
2023-11-27 21:09:42 -0600

    marquee repeating

---
+++ src/lib/components/marquee/Marquee.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Marquee from 'svelte-fast-marquee';
+
+	export let screenWidth: number;
+	let childWidth: number;
+	$: repeatedChildNumber = Math.floor(screenWidth / childWidth) + 2;
+</script>
+
+<Marquee class="py-2 bg-black text-white font-semibold">
+	<!-- have one constant text to get the width, there is a slight delay when rendering the rest of the text, need transition to mask the delay  -->
+	<p class="max-w-fit" bind:clientWidth={childWidth}>APPLY NOW • JANUARY 27 - 28, 2024 •&nbsp;</p>
+	{#if childWidth}
+		{#each { length: repeatedChildNumber } as _, i}
+			<p class="max-w-fit">APPLY NOW • JANUARY 27 - 28, 2024 •&nbsp;</p>
+		{/each}
+	{/if}
+</Marquee>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,17 @@
 <script lang="ts">
-	import Marquee from 'svelte-fast-marquee';
 	import Title from '$lib/components/landing/Title.svelte';
 	import PhysicsContainer from '$lib/components/landing/PhysicsContainer.svelte';
 	import Navbar from '$lib/components/common/Navbar.svelte';
+	import Marquee from '$lib/components/marquee/Marquee.svelte';
+
+	let screenWidth: number;
 </script>
+
+<svelte:window bind:innerWidth={screenWidth} />
 
 <div class="flex flex-col h-dscreen w-full gap-32 font-poppins">
 	<Navbar />
-	<Marquee class="py-2 bg-black text-white font-semibold">
-		<p>APPLY NOW • JANUARY 27 - 28, 2024 •&nbsp;</p>
-		<p>APPLY NOW • JANUARY 27 - 28, 2024 •&nbsp;</p>
-		<p>APPLY NOW • JANUARY 27 - 28, 2024 •&nbsp;</p>
-		<p>APPLY NOW • JANUARY 27 - 28, 2024 •&nbsp;</p>
-		<p>APPLY NOW • JANUARY 27 - 28, 2024 •&nbsp;</p>
-		<p>APPLY NOW • JANUARY 27 - 28, 2024 •&nbsp;</p>
-		<p>APPLY NOW • JANUARY 27 - 28, 2024 •&nbsp;</p>
-		<p>APPLY NOW • JANUARY 27 - 28, 2024 •&nbsp;</p>
-		<p>APPLY NOW • JANUARY 27 - 28, 2024 •&nbsp;</p>
-	</Marquee>
+	<Marquee {screenWidth} />
 	<div class="h-1/2">
 		<Title />
 		<PhysicsContainer />
