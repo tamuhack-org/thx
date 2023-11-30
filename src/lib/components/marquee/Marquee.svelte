8d1d17fabc93133fb35d23df2bbfbc317c79bb61
Adam Teo <adam.teo321@gmail.com>
2023-11-30 17:51:35 -0600

    marquee speed

--- src/lib/components/marquee/Marquee.svelte
+++ src/lib/components/marquee/Marquee.svelte
@@ -6,7 +6,7 @@
 	$: repeatedChildNumber = Math.floor(screenWidth / childWidth) + 2;
 </script>
 
-<Marquee class="py-2 bg-black text-white font-semibold">
+<Marquee class="py-2 bg-black text-white font-semibold" speed={30}>
 	<!-- have one constant text to get the width, there is a slight delay when rendering the rest of the text, need transition to mask the delay  -->
 	<p class="max-w-fit" bind:clientWidth={childWidth}>APPLY NOW • JANUARY 27 - 28, 2024 •&nbsp;</p>
 	{#if childWidth}
