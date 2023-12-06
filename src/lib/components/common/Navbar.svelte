8ca4167b3fce65a23490d164e289721de9349a2f
ysadamt <adam.teo321@gmail.com>
2023-12-06 13:59:07 -0600

    drop up open arrow animation

--- src/lib/components/common/Navbar.svelte
+++ src/lib/components/common/Navbar.svelte
@@ -3,8 +3,16 @@
 	import NavButton from './NavButton.svelte';
 	import { onMount } from 'svelte';
 	import { fly } from 'svelte/transition';
-	import { IconChevronDown, IconChevronUp } from '@tabler/icons-svelte';
-	import ResourcesButton from './ResourcesButton.svelte';
+	import {
+		IconBrandDiscord,
+		IconBrandSpotify,
+		IconChevronDown,
+		IconHelp,
+		IconLicense,
+		IconNotebook,
+		IconScale
+	} from '@tabler/icons-svelte';
+	// import ResourcesButton from './ResourcesButton.svelte';
 
 	let loaded = false;
 	let open = false;
@@ -26,19 +34,42 @@
 		}}
 		class="flex w-full justify-center fixed bottom-8 font-poppins"
 	>
-		<div class="flex flex-col justify-center">
+		<div class="flex flex-col justify-center w-max">
 			<div
 				class:active
-				class="flex flex-col items-center justify-center gap-[6px] bg-black bg-opacity-70 backdrop-blur rounded-t-xl text-white expand-container px-[6px]"
+				class="flex flex-col items-center justify-center gap-[6px] bg-black bg-opacity-70 backdrop-blur rounded-t-xl expand-container px-[6px]"
 			>
 				<div
-					class="flex flex-col bg-[#3e3e3e] mx-[100px] mt-[6px] bg-opacity-75 backdrop-blur rounded-lg w-full h-full"
+					class="flex flex-row items-center bg-[#3e3e3e] mt-[6px] bg-opacity-75 px-6 backdrop-blur rounded-lg w-full h-full text-white text-sm text-left gap-16"
 				>
-					<h1>Hacker Guide</h1>
-					<h1>Discord</h1>
-					<h1>Spotify Queue</h1>
-					<h1>Devpost</h1>
-					<h1>HelpR</h1>
+					<div class="flex flex-col gap-3">
+						<span class="flex flex-row gap-2">
+							<IconNotebook class="w-5 h-5" />
+							<a href="/">Hacker Guide</a>
+						</span>
+						<span class="flex flex-row gap-2">
+							<IconBrandDiscord class="w-5 h-5" />
+							<a href="/">Discord</a>
+						</span>
+						<span class="flex flex-row gap-2">
+							<IconBrandSpotify class="w-5 h-5" />
+							<a href="/">Spotify Queue</a>
+						</span>
+					</div>
+					<div class="flex flex-col gap-3">
+						<span class="flex flex-row gap-2">
+							<IconScale class="w-5 h-5" />
+							<a href="/">Devpost</a>
+						</span>
+						<span class="flex flex-row gap-2">
+							<IconHelp class="w-5 h-5" />
+							<a href="/">HelpR</a>
+						</span>
+						<span class="flex flex-row gap-2">
+							<IconLicense class="w-5 h-5" />
+							<a href="/">Code of Conduct</a>
+						</span>
+					</div>
 				</div>
 			</div>
 			<div
@@ -57,15 +88,13 @@
 						<NavButton text="Prizes" />
 					</div>
 					<button
-						class="flex flex-row justify-center items-center h-[60px] px-5 rounded-xl bg-black text-white font-semibold text-sm"
+						class="flex flex-row justify-center items-center h-[60px] px-5 rounded-xl bg-black text-white font-semibold text-sm gap-2"
 						on:click={() => (open = !open)}
 					>
 						<p>Resources</p>
-						{#if open}
-							<IconChevronUp class="w-5 h-5 ml-2" />
-						{:else}
-							<IconChevronDown class="w-5 h-5 ml-2" />
-						{/if}
+						<span class:arrow-open={open} class:arrow-close={!open} class="flip">
+							<IconChevronDown class="w-5 h-5" />
+						</span>
 					</button>
 				</div>
 			</div>
@@ -83,6 +112,34 @@
 	}
 
 	.expand-container.active {
-		height: 150px;
+		height: 140px;
+	}
+
+	.arrow-open {
+		animation: flip-icon 0.5s ease-in-out;
+		transform: rotateX(180deg);
+	}
+
+	.arrow-close {
+		animation: flip-icon-close 0.5s ease-in-out;
+		transform: rotateX(0deg);
+	}
+
+	@keyframes flip-icon {
+		0% {
+			transform: rotateX(0deg);
+		}
+		100% {
+			transform: rotateX(180deg);
+		}
+	}
+
+	@keyframes flip-icon-close {
+		0% {
+			transform: rotateX(180deg);
+		}
+		100% {
+			transform: rotateX(0deg);
+		}
 	}
 </style>
