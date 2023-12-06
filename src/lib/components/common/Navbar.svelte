d11f865df45acb3a1db6bcd9820626f6488dfd44
Adam Teo <ysadamt@Adams-MacBook-Pro.local>
2023-12-06 00:25:48 -0600

    navbar drop up

--- src/lib/components/common/Navbar.svelte
+++ src/lib/components/common/Navbar.svelte
@@ -3,9 +3,22 @@
 	import NavButton from './NavButton.svelte';
 	import { onMount } from 'svelte';
 	import { fly } from 'svelte/transition';
+	import { IconChevronDown, IconChevronUp } from '@tabler/icons-svelte';
 	import ResourcesButton from './ResourcesButton.svelte';
 
 	let loaded = false;
+	let open = false;
+	let resources = [
+		{
+			name: 'Link 1',
+			link: '#'
+		},
+		{
+			name: 'Link 2',
+			link: '#'
+		}
+	];
+	$: active = open ? 'active' : '';
 
 	onMount(() => {
 		loaded = true;
@@ -21,22 +34,65 @@
 			opacity: 1,
 			delay: 500
 		}}
-		class="w-screen fixed bottom-8 font-poppins"
+		class="flex w-full justify-center fixed bottom-8 font-poppins"
 	>
-		<div class="flex justify-center">
+		<div class="flex flex-col justify-center">
 			<div
-				class="flex items-center bg-black bg-opacity-70 backdrop-blur gap-[2px] px-[6px] rounded-xl"
+				class:active
+				class="flex flex-col items-center justify-center gap-[6px] bg-black bg-opacity-70 backdrop-blur rounded-t-xl text-white expand-container px-[6px]"
 			>
-				<CTANavButton text="Apply" />
 				<div
-					class="flex p-[6px] m-[6px] gap-[6px] bg-[#3e3e3e] bg-opacity-75 backdrop-blur rounded-lg"
+					class="flex flex-col bg-[#3e3e3e] mx-[100px] mt-[6px] bg-opacity-75 backdrop-blur rounded-lg w-full h-full"
 				>
-					<NavButton text="Schedule" />
-					<NavButton text="FAQ" />
-					<NavButton text="Prizes" />
+					<h1>Hacker Guide</h1>
+					<h1>Discord</h1>
+					<h1>Spotify Queue</h1>
+					<h1>Devpost</h1>
+					<h1>HelpR</h1>
 				</div>
-				<ResourcesButton />
+			</div>
+			<div
+				class:rounded-t-xl={!open}
+				class:transition-all={!open}
+				class:delay-[450ms]={!open}
+				class="flex flex-col bg-black bg-opacity-70 backdrop-blur p-[6px] gap-[6px] rounded-b-xl"
+			>
+				<div class="flex items-center">
+					<CTANavButton text="Apply" />
+					<div
+						class="flex p-[6px] gap-[6px] mx-[6px] bg-[#3e3e3e] bg-opacity-75 backdrop-blur rounded-lg"
+					>
+						<NavButton text="Schedule" />
+						<NavButton text="FAQ" />
+						<NavButton text="Prizes" />
+					</div>
+					<button
+						class="flex flex-row justify-center items-center h-[60px] px-5 rounded-xl bg-black text-white font-semibold text-sm"
+						on:click={() => (open = !open)}
+					>
+						<p>Resources</p>
+						{#if open}
+							<IconChevronUp class="w-5 h-5 ml-2" />
+						{:else}
+							<IconChevronDown class="w-5 h-5 ml-2" />
+						{/if}
+					</button>
+				</div>
 			</div>
 		</div>
 	</div>
 {/if}
+
+<style>
+	.expand-container {
+		position: relative;
+		overflow: hidden;
+		height: 0px;
+		transition: height 0.5s ease;
+		text-align: center;
+	}
+
+	.expand-container.active {
+		height: 150px;
+	}
+</style>
