2bca6b8d7b5dff26213fac2cfe8d348ed1e894d7
Abhishek More <abhimore74@gmail.com>
2024-01-03 19:48:32 -0500

    responsive

--- src/lib/components/schedule/Schedule.svelte
+++ src/lib/components/schedule/Schedule.svelte
@@ -84,7 +84,7 @@
 	});
 </script>
 
-<p class="text-center font-medium text-[30px] text-dark font-poppins">Next Up</p>
+<p id="schedule" class="text-center font-medium text-[30px] text-dark font-poppins">Next Up</p>
 <div class="mx-auto w-[50px] h-1 bg-dark rounded-full"></div>
 
 <div class="flex justify-center gap-4 mt-4 md:mt-8 overflow-scroll">
@@ -108,7 +108,7 @@
 	/>
 </div>
 
-<div class="flex flex-col lg:flex-row justify-center xl:justify-start gap-4 mt-8">
+<div class="flex flex-col lg:flex-row justify-center gap-4 mt-8">
 	<div class="relative">
 		<div class="sticky top-0 lg:translate-y-[56px] lg:w-[300px] max-w-[400px] mx-auto">
 			<div class="p-4 border border-dark rounded-md">
