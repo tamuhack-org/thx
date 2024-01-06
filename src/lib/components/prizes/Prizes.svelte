2ad85b5eb2f3a559802d6803157dd4ccdc3a0db2
Abhishek More <abhimore74@gmail.com>
2024-01-05 20:42:18 -0500

    svgs

--- src/lib/components/prizes/Prizes.svelte
+++ src/lib/components/prizes/Prizes.svelte
@@ -1,3 +1,30 @@
-<div class="flex flex-col justify-center items-center h-[1000px] text-white">
-	<h1>prizes todo: abhishek more</h1>
+<div class="flex flex-col min-h-[4000px] text-white container mt-64 w-full">
+	<div class="flex gap-4 w-full">
+		<div class="flex flex-col justify-between items-center w-1/3">
+			<div class="green-gradient w-full h-[250px] rounded-t-md"></div>
+			<div class="flex flex-col justify-between w-full h-[150px] p-4 dark-bg rounded-b-md">
+        <div>
+          <p class="text-2xl font-medium font-poppins copy-on-dark">First Place</p>
+          <p class="text-xl font-light font-poppins copy-on-dark">1st place overall prize.</p>
+        </div>
+        <p class="text-xl font-poppins copy-on-dark">Prize: Electric Scooter</p>
+			</div>
+		</div>
+		<div class="flex justify-between items-center border-2 w-1/3 h-[150px] rounded-md"></div>
+		<div class="flex justify-between items-center border-2 w-1/3 h-[150px] rounded-md"></div>
+	</div>
 </div>
+
+<style>
+	.dark-bg {
+		background-color: #242232;
+	}
+
+  .copy-on-dark {
+    color: rgb(223 223 223);
+  }
+
+	.green-gradient {
+		background: linear-gradient(180deg, #d8f9e9 0.09%, #fff 151.34%);
+	}
+</style>
